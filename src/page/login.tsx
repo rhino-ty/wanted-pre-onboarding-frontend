e487bd4cf0554099a32d1b556521192065500476
Rhino-ty <okko8522@gmail.com>
2023-04-11 16:34:20 +0900

    design: 로그인 페이지 mui로 디자인 수정

--- src/page/login.tsx
+++ src/page/login.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { postLogin } from "../api/auth/loginApi";
 import { useRouter } from "../hooks/useRouter";
 import { getAccessTokenFromLocalStorage } from "../utils/accessTokenHandler";
-
-// import { saveRefreshTokenToLocalStorage } from "@/src/utils/refreshTokenHandler";
 // import { saveAccessTokenToLocalStorage } from "@/src/utils/accessTokenHandler";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 export default function Login() {
   const { routeTo } = useRouter();
@@ -40,113 +47,66 @@ export default function Login() {
     routeTo("/todo");
   };
 
-  const handleSignup = () => {
-    routeTo("/signup");
-  };
-
   const isButtonDisabled = !email.includes("@") || !email.includes(".") || password.length < 8;
 
   return (
-    <S.Container>
-      <form onSubmit={handleSubmit}>
-        <S.AccountWrapper>
-          <S.InputField>
-            <input
-              type="email"
-              placeholder="이메일"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </S.InputField>
-          <S.InputField>
-            <input
-              type="password"
-              placeholder="비밀번호"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </S.InputField>
-        </S.AccountWrapper>
-
-        <S.ButtonWrapper>
-          <S.LoginButton disabled={isButtonDisabled} type="submit">
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          로그인
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="이메일"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="비밀번호"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={isButtonDisabled}
+          >
             로그인
-          </S.LoginButton>
-          <S.RegisterButton onClick={handleSignup}>회원가입</S.RegisterButton>
-        </S.ButtonWrapper>
-      </form>
-    </S.Container>
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                {"계정이 없다면 회원가입하기"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
-
-const S = {
-  Container: styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    max-width: 768px;
-    margin: auto;
-  `,
-
-  AccountWrapper: styled.div`
-    margin: auto;
-  `,
-
-  InputField: styled.div`
-    margin: 0 auto;
-    width: 452px;
-    height: 50px;
-    border: 1px solid #dcdce0;
-    border-radius: 8px;
-    margin-bottom: 18px;
-    display: flex;
-    align-items: center;
-
-    & > input {
-      border: none;
-      padding: 0 16px;
-      border-radius: 8px;
-      height: 100%;
-      flex: 1;
-
-      &::placeholder {
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 140%;
-        color: #959599;
-      }
-    }
-  `,
-
-  ButtonWrapper: styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  `,
-
-  LoginButton: styled.button`
-    width: 452px;
-    height: 50px;
-    background-color: ${({ disabled }) => (disabled ? "#eee" : "#ff812e")};
-    color: ${({ disabled }) => (disabled ? "#959599" : "#fff")};
-    border-radius: 8px;
-    margin: 0 auto;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 22.4px;
-    cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
-  `,
-
-  RegisterButton: styled.button`
-    margin: 20px;
-    width: 56px;
-    border-bottom: 1px solid #49494d;
-    color: #49494d;
-    font-weight: 800;
-    font-size: 16px;
-    line-height: 20px;
-    cursor: pointer;
-  `,
-};
