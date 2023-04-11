e6cc4aabae9106a168c5c97458283882b398c1a0
Rhino-ty <okko8522@gmail.com>
2023-04-11 16:24:56 +0900

    refactor: 삼항연산자 뺌, export 제거
    getAccessTokenFromLocalStorage 조건 문을 없앰

--- src/api/axiosModul.ts
+++ src/api/axiosModul.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 import { getAccessTokenFromLocalStorage } from "../utils/accessTokenHandler";
 
-export const SERVER_URL = `https://www.pre-onboarding-selection-task.shop/`;
+const SERVER_URL = "https://www.pre-onboarding-selection-task.shop/";
 
 const instance = axios.create({
   baseURL: SERVER_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: !!getAccessTokenFromLocalStorage()
-      ? `Bearer ${getAccessTokenFromLocalStorage()}`
-      : null,
+    Authorization: `Bearer ${getAccessTokenFromLocalStorage() || ""}`,
   },
 });
 
