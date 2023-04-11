19e466206ad6560dceec9769f2a9958d2640c3ff
Rhino-ty <okko8522@gmail.com>
2023-04-11 15:17:24 +0900

    feat: RouterProvider로 배열 routers 라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import routers from "./router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={routers} />
+    </div>
+  );
 }
 
 export default App;
