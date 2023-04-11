4e349abb1b76eab517caf1051fb530dbd6573456
Rhino-ty <okko8522@gmail.com>
2023-04-11 14:55:28 +0900

    feat: 라우터 구현
    withAuth를 기준으로 나눠 페이지 분리 및 hasAccessToken로 토큰 없으면 /login으로 리다이렉트

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,8 @@
-import { SidebarElement } from "./types/sidebar";
-import { createBrowserRouter } from "react-router-dom";
-// import { Router as RemixRouter } from "@remix-run/router/dist/router";
-// import GeneralLayout from "./layout/GeneralLayout";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import PageA from "./pages/PageA";
-// import PageB from "./pages/PageB";
-// import PageC from "./pages/PageC";
+import { createBrowserRouter, RouteObject, Navigate } from "react-router-dom";
+import Login from "./page/login";
+import Signup from "./page/signup";
+// import Todo from "./page/todo";
+import { getAccessTokenFromLocalStorage } from "./utils/accessTokenHandler";
 
 interface RouterElement {
   id: number; // 페이지 아이디 (반복문용 고유값)
@@ -14,44 +10,58 @@ interface RouterElement {
   label: string; // 사이드바에 표시할 페이지 이름
   element: React.ReactNode; // 페이지 엘리먼트
   withAuth?: boolean; // 인증이 필요한 페이지 여부
+  redirectPath?: string;
 }
 
 const routerData: RouterElement[] = [
-  // TODO 3-1: 로그인 페이지 라우터 등록하기 ('login', withAuth: false)
-  // TODO 3-2: page a, b, c 등록하기
   {
     id: 0,
-    path: "/",
-    label: "Home",
-    element: <Home />,
-    withAuth: false,
-  },
-  {
-    id: 1,
     path: "/login",
     label: "Login",
     element: <Login />,
     withAuth: false,
+    redirectPath: "/todo",
   },
   {
-    id: 2,
-    path: "/page-a",
-    label: "PageA",
-    element: <PageA />,
-    withAuth: true,
+    id: 1,
+    path: "/signup",
+    label: "Signup",
+    element: <Signup />,
+    withAuth: false,
+    redirectPath: "/todo",
   },
-  {
-    id: 3,
-    path: "/page-b",
-    label: "PageB",
-    element: <PageB />,
-    withAuth: true,
-  },
-  {
-    id: 4,
-    path: "/page-c",
-    label: "PageC",
-    element: <PageC />,
-    withAuth: true,
-  },
+  // {
+  //   id: 2,
+  //   path: "/todo",
+  //   label: "Todo",
+  //   element: <Todo />,
+  //   withAuth: true,
+  // },
 ];
+
+const routers = createBrowserRouter(
+  routerData.map((router) => {
+    const hasAccessToken = !!getAccessTokenFromLocalStorage();
+    const isProtectedRoute = !!router.withAuth;
+    const isAuthPage = !!(router.path === "/login" || router.path === "/signup");
+
+    if (isProtectedRoute) {
+      return {
+        path: router.path,
+        element: hasAccessToken ? router.element : <Navigate to={isAuthPage ? "/" : "/login"} />,
+      } as RouteObject;
+    }
+
+    return {
+      path: router.path,
+      element:
+        hasAccessToken && router.redirectPath ? (
+          <Navigate to={router.redirectPath} />
+        ) : (
+          router.element
+        ),
+    } as RouteObject;
+  })
+);
+
+export default routers;
