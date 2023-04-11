9011baf388fdbbae5fc37f00b60270b37eca64cb
Rhino-ty <okko8522@gmail.com>
2023-04-11 17:43:24 +0900

    refactor: 변수 명 수정

--- src/utils/accessTokenHandler.ts
+++ src/utils/accessTokenHandler.ts
@@ -1,11 +1,11 @@
 export const saveAccessTokenToLocalStorage = (accessToken: string) => {
   if (typeof window !== "undefined") {
-    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("access_token", accessToken);
   }
 };
 
 export const getAccessTokenFromLocalStorage = () => {
   if (typeof window !== "undefined") {
-    return localStorage.getItem("accessToken") || "";
+    return localStorage.getItem("access_token") || "";
   }
 };
