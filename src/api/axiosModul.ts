834694cf79e17cac7f014ef3fdcc66aec2bcd637
Rhino-ty <okko8522@gmail.com>
2023-04-11 15:42:52 +0900

    feat: axios instance header 수정
    Content-Type 추가

--- src/api/axiosModul.ts
+++ src/api/axiosModul.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { getAccessTokenFromLocalStorage } from "../utils/accessTokenHandler";
 
 export const SERVER_URL = `https://www.pre-onboarding-selection-task.shop/`;
 
 const instance = axios.create({
   baseURL: SERVER_URL,
-  // headers: {
-  //   Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYWJAYWEuY29tIiwiQVVUSCI6IlJPTEVfVVNFUiIsImV4cCI6MjAwMzM4OTE0MH0.mD1FqQkgMTKZB-Pkwd1qWIWMK4tl_hGInEDG1aP7rB7ZZOJIlj6AmlCxJvwJSXTtui7vl3bpyMLf__v_gJyTnA`,
-  // },
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: !!getAccessTokenFromLocalStorage()
+      ? `Bearer ${getAccessTokenFromLocalStorage()}`
+      : null,
+  },
 });
 
 export default instance;
