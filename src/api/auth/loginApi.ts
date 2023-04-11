46d8e144485dc137c0a6da170a3b2ded6adebcab
Rhino-ty <okko8522@gmail.com>
2023-04-11 15:44:06 +0900

    feat: 로그인 api 수정
    성공시 로컬스토리지에 액세스토큰 save, 결과에 따라 success, fail 리턴

--- src/api/auth/loginApi.ts
+++ src/api/auth/loginApi.ts
@@ -1,6 +1,24 @@
+import { saveAccessTokenToLocalStorage } from "../../utils/accessTokenHandler";
 import instance from "../axiosModul";
 
-export const postLogin = async (email: string, password: string) => {
-  const response = await instance.post("/auth/signin", { email, password });
-  return response.data;
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+type LoginResult = "success" | "fail";
+
+export const postLogin = async (args: LoginRequest): Promise<LoginResult> => {
+  const loginRes = await instance.post(`/auth/login`, {
+    body: args,
+  });
+  const { data: loginResponseData } = loginRes;
+
+  saveAccessTokenToLocalStorage(loginResponseData.access_token);
+
+  if (loginResponseData.ok) {
+    return "success";
+  }
+
+  return "fail";
 };
