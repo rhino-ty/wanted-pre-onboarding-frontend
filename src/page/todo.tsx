5377aafe3134bf9e06f6287b5d6e10f72a8c9b4b
Rhino-ty <okko8522@gmail.com>
2023-04-13 00:50:07 +0900

    design: 대충 CSS 구현
    중앙 정렬, 내용에 따라 width 늘어나기 등등

--- src/page/todo.tsx
+++ src/page/todo.tsx
@@ -3,6 +3,7 @@ import { Todo, createTodo, deleteTodo, getTodos, updateTodo } from "../api/todo/
 import CssBaseline from "@mui/material/CssBaseline";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import styled from "styled-components";
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -54,82 +55,146 @@ export default function TodoList() {
   return (
     <>
       <CssBaseline />
-      <main>
-        <h1>Todo List</h1>
-        <ul>
-          {todos.length !== 0 ? (
-            todos.map((todo) =>
-              todo.isEditMode ? (
-                <li key={todo.id}>
-                  <TextField
-                    data-testid="modify-input"
-                    type="text"
-                    value={todo.todo}
-                    onChange={(e) =>
-                      setTodos((prevTodos) =>
-                        prevTodos.map((prevTodo) =>
-                          prevTodo.id === todo.id ? { ...prevTodo, todo: e.target.value } : prevTodo
+      <TodoListContainer>
+        <Main>
+          <ListTitle>Todo List</ListTitle>
+          <ListUl>
+            {todos.length !== 0 ? (
+              todos.map((todo) =>
+                todo.isEditMode ? (
+                  <ListLi key={todo.id}>
+                    <ListTextField
+                      data-testid="modify-input"
+                      type="text"
+                      value={todo.todo}
+                      onChange={(e) =>
+                        setTodos((prevTodos) =>
+                          prevTodos.map((prevTodo) =>
+                            prevTodo.id === todo.id
+                              ? { ...prevTodo, todo: e.target.value }
+                              : prevTodo
+                          )
                         )
-                      )
-                    }
-                  />
-                  <Button
-                    data-testid="submit-button"
-                    onClick={() => handleTodoUpdate(todo.id, todo.todo, todo.isCompleted)}
-                  >
-                    제출
-                  </Button>
-                  <Button
-                    data-testid="cancel-button"
-                    onClick={() =>
-                      setTodos((prevTodos) =>
-                        prevTodos.map((prevTodo) =>
-                          prevTodo.id === todo.id ? { ...prevTodo, isEditMode: false } : prevTodo
-                        )
-                      )
-                    }
-                  >
-                    취소
-                  </Button>
-                </li>
-              ) : (
-                <li key={todo.id}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      checked={todo.isCompleted}
-                      onChange={(e) => handleTodoUpdate(todo.id, todo.todo, e.target.checked)}
+                      }
                     />
-                    <span>{todo.todo}</span>
-                  </label>
-                  <Button data-testid="modify-button" onClick={() => handleEditMode(todo.id)}>
-                    수정
-                  </Button>
-                  <Button data-testid="delete-button" onClick={() => handleTodoDelete(todo.id)}>
-                    삭제
-                  </Button>
-                </li>
+                    <ListButton
+                      data-testid="submit-button"
+                      onClick={() => handleTodoUpdate(todo.id, todo.todo, todo.isCompleted)}
+                    >
+                      제출
+                    </ListButton>
+                    <ListButton
+                      data-testid="cancel-button"
+                      onClick={() =>
+                        setTodos((prevTodos) =>
+                          prevTodos.map((prevTodo) =>
+                            prevTodo.id === todo.id ? { ...prevTodo, isEditMode: false } : prevTodo
+                          )
+                        )
+                      }
+                    >
+                      취소
+                    </ListButton>
+                  </ListLi>
+                ) : (
+                  <ListLi key={todo.id}>
+                    <ListCheckboxLabel>
+                      <ListCheckbox
+                        type="checkbox"
+                        checked={todo.isCompleted}
+                        onChange={(e) => handleTodoUpdate(todo.id, todo.todo, e.target.checked)}
+                      />
+                      <ListTitle>{todo.todo}</ListTitle>
+                    </ListCheckboxLabel>
+                    <ListButton data-testid="modify-button" onClick={() => handleEditMode(todo.id)}>
+                      수정
+                    </ListButton>
+                    <ListButton
+                      data-testid="delete-button"
+                      onClick={() => handleTodoDelete(todo.id)}
+                    >
+                      삭제
+                    </ListButton>
+                  </ListLi>
+                )
               )
-            )
-          ) : (
-            <span>할 일이 없습니다.</span>
-          )}
-        </ul>
-        <div>
-          <TextField
-            data-testid="new-todo-input"
-            type="text"
-            value={newTodo}
-            onChange={(e) => {
-              setNewTodo(e.target.value);
-            }}
-            onKeyUp={handleNewTodoEnterPress}
-          />
-          <Button data-testid="new-todo-add-button" onClick={handleNewTodoSubmit}>
-            추가
-          </Button>
-        </div>
-      </main>
+            ) : (
+              <ListTitle>할 일이 없습니다.</ListTitle>
+            )}
+          </ListUl>
+          <TodoListContainer>
+            <ListTextField
+              data-testid="new-todo-input"
+              type="text"
+              value={newTodo}
+              onChange={(e) => {
+                setNewTodo(e.target.value);
+              }}
+              onKeyPress={handleNewTodoEnterPress}
+            />
+            <ListButton data-testid="new-todo-add-button" onClick={handleNewTodoSubmit}>
+              추가
+            </ListButton>
+          </TodoListContainer>
+        </Main>
+      </TodoListContainer>
     </>
   );
 }
+
+const TodoListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f5f5f5;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 32px;
+`;
+
+const ListTitle = styled.h1`
+  font-size: 1rem;
+`;
+
+const ListUl = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const ListLi = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 8px;
+  padding: 8px;
+  background-color: #fff;
+  border-radius: 8px;
+`;
+
+const ListCheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 1.2rem;
+`;
+
+const ListCheckbox = styled.input`
+  margin-right: 16px;
+  cursor: pointer;
+`;
+
+const ListTextField = styled(TextField)`
+  margin-right: 16px;
+`;
+
+const ListButton = styled(Button)`
+  margin-right: 8px;
+`;
