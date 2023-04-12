d63ebd1ac644304cf9fc151f8257efff6256e1a8
Rhino-ty <okko8522@gmail.com>
2023-04-12 23:17:40 +0900

    feat: get 해오기 기능 구현

--- src/page/todo.tsx
+++ src/page/todo.tsx
@@ -1,3 +1,32 @@
-export default function todo() {
-  return <>todo</>;
+import { useEffect, useState } from "react";
+import { Todo, getTodos } from "../api/todo/todoAPI";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const fetchedTodos = await getTodos();
+      setTodos(fetchedTodos);
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <ul>
+      {todos.length !== 0 ? (
+        todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.isCompleted} />
+              <span>{todo.todo}</span>
+            </label>
+          </li>
+        ))
+      ) : (
+        <span>할 일이 없습니다.</span>
+      )}
+    </ul>
+  );
 }
