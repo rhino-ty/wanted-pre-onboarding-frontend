9f926c1212fe02d2b91c52a8d0c582477259967a
Rhino-ty <okko8522@gmail.com>
2023-04-12 23:28:16 +0900

    feat: 추가 기능 구현
    newTodo 상태 추가, 제출 후 실시간 반영 되게 setTodos로 추가 후 통신한 뒤 값인 createdTodo 추가

--- src/page/todo.tsx
+++ src/page/todo.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { Todo, getTodos } from "../api/todo/todoAPI";
+import { Todo, createTodo, getTodos } from "../api/todo/todoAPI";
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -13,20 +14,45 @@ export default function TodoList() {
     fetchTodos();
   }, []);
 
+  const handleNewTodoSubmit = async () => {
+    if (newTodo.trim() === "") {
+      return;
+    }
+    const createdTodo = await createTodo(newTodo);
+    setTodos((prevTodos) => [...prevTodos, createdTodo]);
+    setNewTodo("");
+  };
+
   return (
-    <ul>
-      {todos.length !== 0 ? (
-        todos.map((todo) => (
-          <li key={todo.id}>
-            <label>
-              <input type="checkbox" checked={todo.isCompleted} />
-              <span>{todo.todo}</span>
-            </label>
-          </li>
-        ))
-      ) : (
-        <span>할 일이 없습니다.</span>
-      )}
-    </ul>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.length !== 0 ? (
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.isCompleted} />
+                <span>{todo.todo}</span>
+              </label>
+            </li>
+          ))
+        ) : (
+          <span>할 일이 없습니다.</span>
+        )}
+      </ul>
+      <div>
+        <input
+          data-testid="new-todo-input"
+          type="text"
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+        />
+        <button data-testid="new-todo-add-button" onClick={handleNewTodoSubmit}>
+          추가
+        </button>
+      </div>
+    </div>
   );
 }
